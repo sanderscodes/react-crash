318c488cbc75ffac3981e0abb11bb42a74ba08b8
sanderscodes <ricesoup@yahoo.com>
2024-04-14 13:25:39 -0400

    added homecards and card and props

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 
 
-const Hero = () => {
+const Hero = ({ title = 'Become a React Dev', subtitle = 'Find the React job that fits your skill set' }) => {
   return (
     <section className="bg-indigo-700 py-20 mb-4">
     <div
@@ -10,15 +10,15 @@ const Hero = () => {
         <h1
           className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl"
         >
-          Become a React Dev
+          {title}
         </h1>
         <p className="my-4 text-xl text-white">
-          Find the React job that fits your skills and needs
+          {subtitle}
         </p>
       </div>
     </div>
   </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
