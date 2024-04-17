d9d8991fd38f813162ea325ce74c156be457a71e
sanderscodes <ricesoup@yahoo.com>
2024-04-17 14:46:03 -0400

    added react icon and router

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,10 @@
-import Navbar from "./components/Navbar";
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, } from 'react-router-dom'
+import HomePage from './components/pages/HomePage';
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 const App = () => {
-  return (
-    <>
-    <Navbar/>
-     <Hero  />
-     <HomeCards />
-     <JobListings />
-     <ViewAllJobs /> 
-
-  
-
-    
-   
-    
-   
-
-   
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
