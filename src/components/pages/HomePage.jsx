1069faf9ec504090ab992d3cf65e324a9418a6bc
sanderscodes <ricesoup@yahoo.com>
2024-04-22 15:15:36 -0400

    added mainlayout with outlet

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import Hero from '../Hero';
-
-
+import HomeCards from '../HomeCards';
+import JobListings from '../JobListings';
+import ViewAllJobs from '../ViewAllJobs'; 
 
 const HomePage = () => {
   return (
     <>
     <Hero />
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs /> 
     </>
   );
 };
