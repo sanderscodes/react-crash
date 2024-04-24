2ec4135dec632d4b62389883d0cd220e885a4a0e
sanderscodes <ricesoup@yahoo.com>
2024-04-23 23:08:37 -0400

    added jobspage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider, } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import HomePage from './components/pages/HomePage';
+import JobsPage from './components/pages/JobsPage';
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
   <Route index element={<HomePage />} />
+  <Route path='/jobs' element={<JobsPage />} />
   </Route>
   )
 );
