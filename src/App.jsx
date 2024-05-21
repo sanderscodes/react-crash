bd31da6ccc7233f9fcbabae44477606e4ab7e9e5
sanderscodes <ricesoup@yahoo.com>
2024-05-21 18:38:16 -0400

    add jobpage

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const router = createBrowserRouter(
   <Route path='/' element={<MainLayout />}>
   <Route index element={<HomePage />} />
   <Route path='/jobs' element={<JobsPage />} />
-  <Route path='/jobs' element={<AddJobPage />} />
+  <Route path='/add-job' element={<AddJobPage />} />
   <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
   <Route path='*' element={<NotFoundPage />} />
   </Route>
