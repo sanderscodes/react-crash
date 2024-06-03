46baaeb230bec5a38976d8ed26935a1263e9fe5d
sanderscodes <ricesoup@yahoo.com>
2024-06-03 15:23:40 -0400

    edit app and addjobpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,22 @@ import JobsPage from './components/pages/JobsPage';
 import NotFoundPage from './components/pages/NotFoundPage';
 import JobPage, { jobLoader } from './components/pages/JobPage';
 import AddJobPage from './components/pages/AddJobPage';
-const router = createBrowserRouter(
-  createRoutesFromElements(
-  <Route path='/' element={<MainLayout />}>
-  <Route index element={<HomePage />} />
-  <Route path='/jobs' element={<JobsPage />} />
-  <Route path='/add-job' element={<AddJobPage />} />
-  <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-  <Route path='*' element={<NotFoundPage />} />
-  </Route>
-  )
-);
+
 const App = () => {
+  const addJob = (newJob) => {
+    console.log(newJob);
+     };
+    const router = createBrowserRouter(
+      createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
+      <Route path='*' element={<NotFoundPage />} />
+      </Route>
+      )
+    );
   return <RouterProvider router={router} />;
 };
 
